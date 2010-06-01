0c8e27603c4ab32928e301c279235bbcd14d563a
Kevin Daudt <compufreak@gmail.com>
2010-06-02 01:28:33 +0800

    Made modelname parameter in factory method optional

--- classes/automodeler.php
+++ classes/automodeler.php
@@ -127,7 +127,7 @@ class AutoModeler extends Model implements ArrayAccess, Iterator
 	 *
 	 * @return Object
 	 */
-	public static function factory($model, $id = FALSE)
+	public static function factory($model = null, $id = FALSE)
 	{
 		$model = empty($model) ? __CLASS__ : 'Model_'.ucfirst($model);
 		return new $model($id);
