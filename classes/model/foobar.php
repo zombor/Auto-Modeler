d47ffe26822ad591a4a8341913db661ec257b203
Jeremy Bush <contractfrombelow@gmail.com>
2010-08-30 07:27:47 -0500

    adding failing test for #4, find_related() fails on NULL columns

---
+++ classes/model/foobar.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+* Foobar Model
+*
+* @package        AutoModeler
+* @author         Jeremy Bush
+* @copyright      (c) 2010 Jeremy Bush
+* @license        http://www.opensource.org/licenses/isc-license.txt
+*/
+class Model_Foobar extends AutoModeler_ORM {
+
+	protected $_table_name = 'foobars';
+
+	protected $_data = array('id' => '',
+	                         'name' => '');
+
+} // End Model_Foobar
