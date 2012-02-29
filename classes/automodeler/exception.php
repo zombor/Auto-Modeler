13ca2a652134533a9fb0fc1e34f3517cbdf47836
Jeremy Bush <contractfrombelow@gmail.com>
2012-02-28 21:08:36 -0600

    Test that created saved models throws exception

---
+++ classes/automodeler/exception.php
@@ -0,0 +1,6 @@
+<?php
+
+class AutoModeler_Exception extends Exception
+{
+
+}
--- tests/automodeler/dao/database.php
+++ tests/automodeler/dao/database.php
@@ -2,9 +2,13 @@
 
 include 'classes/automodeler/dao/database.php';
 include 'classes/automodeler/model.php';
+include 'classes/automodeler/exception.php';
 
 class Test_AutoModeler_DAO_Database extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * Tests we can create a new, clean record with a new model instance
+	 */
 	public function test_create_new()
 	{
 		// Create database mock
@@ -67,5 +71,51 @@ class Test_AutoModeler_DAO_Database extends PHPUnit_Framework_TestCase
 
 		$this->assertSame(AutoModeler_Model::STATE_LOADED, $new_model->state());
 		$this->assertFalse($new_model->id == NULL);
+	}
+
+	/**
+	 * Tests that already loaded methods throw exceptions when being created
+	 */
+	public function test_create_new_with_loaded_model()
+	{
+		$database = $this->getMock('Database');
+
+		// Create model mock
+		$model = $this->getMock(
+			'AutoModeler_Model',
+			array('state')
+		);
+
+		$model->expects($this->at(0))
+			->method('state')
+			->will(
+				$this->returnValue(AutoModeler_Model::STATE_LOADED)
+			);
+
+		// Create a qb mock
+		$qb = $this->getMock(
+			'Database_Query_Builder_Insert',
+			array('execute', 'values')
+		);
+		$qb->expects($this->any())
+			->method('execute')
+			->will($this->returnValue('foo'));
+		$qb->expects($this->any())
+			->method('values')
+			->will($this->returnValue(NULL));
+
+
+		try
+		{
+			// Try and save the model
+			$dao = new AutoModeler_DAO_Database($database);
+			$new_model = $dao->create($model, $qb);
+
+			$this->fail('Create cannot be called on saved models!');
+		}
+		catch (AutoModeler_Exception $e)
+		{
+
+		}
 	}
 }
