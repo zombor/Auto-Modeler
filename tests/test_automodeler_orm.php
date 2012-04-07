21385c133bc71d467a9d80d55f91f41597a55e4d
Raitis Stengrevics <dagrevis@gmail.com>
2012-04-07 15:26:55 +0300

    Fixed how the configuration is loaded. Now it's loaded in Kohana 3.2 way.

--- tests/test_automodeler_orm.php
+++ tests/test_automodeler_orm.php
@@ -46,7 +46,7 @@ class AutoModeler_ORM_Test extends PHPUnit_Extensions_Database_TestCase
 {
 	protected function getconnection()
 	{
-		$config = Kohana::config('database')->default;
+		$config = Kohana::$config->load('database')->default;
 
 		$pdo = new PDO('mysql:host='.$config['connection']['hostname'].';dbname='.$config['connection']['database'], $config['connection']['username'], $config['connection']['password']);
 		return $this->createDefaultDBConnection($pdo, $config['connection']['database']);
