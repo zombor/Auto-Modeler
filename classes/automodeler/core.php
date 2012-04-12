0784c72890bdb2090efa472e8e2bfc464cab22d7
Juan Treminio <jtreminio@gmail.com>
2012-04-11 23:12:48 -0500

    Moved AutoModeler_Exception into own file

--- classes/automodeler/core.php
+++ classes/automodeler/core.php
@@ -546,19 +546,3 @@ class AutoModeler_Core extends Model_Database implements ArrayAccess
 		$this->_data[$key] = NULL;
 	}
 }
-
-class AutoModeler_Exception extends Kohana_Exception
-{
-	public $errors;
-
-	public function __construct($title, array $message = NULL, $errors = '')
-	{
-		parent::__construct($title, $message);
-		$this->errors = $errors;
-	}
-
-	public function __toString()
-	{
-		return $this->message;
-	}
-}
